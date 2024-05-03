be8ecba6e43236b2f4639b024543ec85ec64211c
Luis Choque <luis.choque.castro@outlook.com>
2024-05-02 22:45:44 -0500

    feat: Chapter history list

--- src/types/user-chapter-history/list.ts
+++ src/types/user-chapter-history/list.ts
@@ -1,7 +1,9 @@
-import { Static, t } from 'elysia';
+import { type Static, t } from 'elysia';
 import { PaginationQuery } from '../common/pagination';
 
 export const UserChapterHistoryListQuery = t.Object({
+    manga_slug: t.Optional(t.String()),
+    include_finished: t.Optional(t.Boolean()),
     ...PaginationQuery.properties,
 });
 
