9bad732a126d78bcef6089c225bdd0d6799c0fbc
ChoqueCastroLD <luis.choque.castro@outlook.com>
2026-02-02 21:41:11 -0500

    feat: unreleased fix

--- src/types/chapter/create.ts
+++ src/types/chapter/create.ts
@@ -2,7 +2,7 @@ import { type Static, t } from 'elysia';
 
 export const CreateChapterRequest = t.Object({
     title: t.String(),
-    releasedAt: t.Date(),
+    releasedAt: t.Optional(t.Union([t.Date(), t.Null()])),
     number: t.Number(),
     image: t.Optional(t.Union([t.String(), t.Null()])),
     pages: t.Optional(t.Array(t.String())),
